79ac548c9edcffc18d7789b4c6f232863a2c809f
aurora12cuadra <aurora12cuadra@gmail.com>
2024-12-17 07:57:43 -0600

    Preparando back para el server

--- backend/app.js
+++ backend/app.js
@@ -25,14 +25,14 @@ app.use('/paciente', pacienteAntecedentesRoutes);
 app.use("/correo", correoRoutes);
 app.use(cors());
 
- app.listen(3000, () => {
-     console.log('Servidor corriendo en http://localhost:3000');
- });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+//app.listen(3000, () => {
+//     console.log('Servidor corriendo en http://localhost:3000');
 // });
+
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => {
+     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+ });
 
 //alter: true //sobreeescribe la bd
 //force: false //crea cada vez la bd 
